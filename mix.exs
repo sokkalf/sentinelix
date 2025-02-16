e7d1d89bbd9db292b1e26d759bf0c149d3df8bd6
Christian Lønaas <christian.lonaas@ugle-z.no>
2025-02-16 18:03:07 +0100

    Add SQLite lib

--- mix.exs
+++ mix.exs
@@ -36,6 +36,7 @@ defmodule Sentinelix.MixProject do
       {:phoenix_ecto, "~> 4.4"},
       {:ecto_sql, "~> 3.6"},
       {:postgrex, ">= 0.0.0"},
+      {:ecto_sqlite3, "~> 0.9.1"},
       {:phoenix_html, "~> 3.3"},
       {:phoenix_live_reload, "~> 1.2", only: :dev},
       {:phoenix_live_view, "~> 0.18.16"},
